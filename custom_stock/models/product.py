75f609f71779125d6b875c8476bd61bb514cf6e8
Tsiky <tsiky@test.com>
2022-12-12 15:40:00 +0300

    iherit product

---
+++ custom_stock/models/product.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Product_template(models.Model):
+    """inherit product template for customization
+
+    Args:
+        models (product_template): custom fields and method
+    """
+    _inherit = "product.template"
+    
+    margin_product = fields.Char('Marge')
