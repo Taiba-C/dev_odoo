9cd1c0c41cb11daa9f380939163238673f49d6b9
Tsiky <tsiky@test.com>
2022-12-12 15:39:35 +0300

    add dependency

--- custom_stock/__manifest__.py
+++ custom_stock/__manifest__.py
@@ -21,13 +21,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','stock'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/product.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
