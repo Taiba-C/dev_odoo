d1652b0bafd5df10ee0490d3d515c68ad182138d
Tsiky <tsiky@test.com>
2022-12-12 16:23:30 +0300

    update manifest

--- custom_stock/__manifest__.py
+++ custom_stock/__manifest__.py
@@ -28,7 +28,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/product.xml',
+        # 'views/product.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
