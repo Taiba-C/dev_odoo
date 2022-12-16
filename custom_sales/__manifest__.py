9dfdac73c18d475dd3eaeb9757a1433d10678ee2
Tsiky <tsiky@test.com>
2022-12-16 09:25:34 +0300

    update manifest

--- custom_sales/__manifest__.py
+++ custom_sales/__manifest__.py
@@ -21,13 +21,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sale','sale_management'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/sale_order.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
