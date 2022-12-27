3024a4b357c4697ff14c25214369ea3333d3bf3d
Tsiky <tsiky@test.com>
2022-12-27 13:52:29 +0300

    rebase

--- custom_stock/models/product.py
+++ custom_stock/models/product.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
 
 class Product_template(models.Model):
     """inherit product template for customization
@@ -11,3 +13,33 @@ class Product_template(models.Model):
     _inherit = "product.template"
     
     margin_product = fields.Float('Taux Com')
+    is_bom_parent = fields.Boolean('Is BOM Parent')
+    
+class Mrp_bom(models.Model):
+    _inherit = "mrp.bom"
+    
+    @api.model
+    def create(self, vals):
+        res = super(Mrp_bom, self).create(vals)
+        
+        self.env['product.template'].search([('id', '=', vals['product_tmpl_id'])]).write({'is_bom_parent': True})
+        
+            
+        return res
+    
+    def unlink(self):
+        
+        self.env['product.template'].search([('id', '=', self.product_tmpl_id)]).write({'is_bom_parent': False})
+        
+        return super(Mrp_bom, self).unlink()
+
+    def write(self, vals):
+        
+        self.env['product.template'].search([('id', '=', self.product_tmpl_id.id)]).write({'is_bom_parent': False})
+
+        res = super(Mrp_bom, self).write(vals)
+        
+        self.env['product.template'].search([('id', '=', vals['product_tmpl_id'])]).write({'is_bom_parent': True})
+  
+        return res  
+    
