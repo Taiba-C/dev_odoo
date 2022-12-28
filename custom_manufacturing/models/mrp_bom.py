518f57b2835a3b68e961ede62323701d38e3fb01
Tsiky <tsiky@test.com>
2022-12-28 10:09:42 +0300

    add description

--- custom_manufacturing/models/mrp_bom.py
+++ custom_manufacturing/models/mrp_bom.py
@@ -9,6 +9,9 @@ class Mrp_bom(models.Model):
     
     @api.model
     def create(self, vals):
+        """
+            change product template and product product as bom parent
+        """
         res = super(Mrp_bom, self).create(vals)
         
         self.env['product.template'].search([('id', '=', vals['product_tmpl_id'])]).write({'is_bom_parent': True})
@@ -18,7 +21,9 @@ class Mrp_bom(models.Model):
         return res
     
     def unlink(self):
-        print(self.product_tmpl_id.id)
+        """
+            change product template and product product as bom parent
+        """
         
         self.env['product.template'].search([('id', '=', self.product_tmpl_id.id)]).write({'is_bom_parent': False})
         self.env['product.product'].search([('product_tmpl_id', '=', self.product_tmpl_id.id)]).write({'is_bom_parent': False})
@@ -26,6 +31,9 @@ class Mrp_bom(models.Model):
         return super(Mrp_bom, self).unlink()
 
     def write(self, vals):
+        """
+            change product template and product product as bom parent
+        """
         
         self.env['product.template'].search([('id', '=', self.product_tmpl_id.id)]).write({'is_bom_parent': False})
         self.env['product.product'].search([('product_tmpl_id', '=', self.product_tmpl_id.id)]).write({'is_bom_parent': False})
