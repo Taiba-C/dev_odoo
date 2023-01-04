8b7a2848a518bb9116aefdb87f1d5a4ca0c966d3
Tsiky <tsiky@test.com>
2023-01-04 16:35:09 +0300

    add dependancy

--- custom_sales/__manifest__.py
+++ custom_sales/__manifest__.py
@@ -21,7 +21,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale','sale_management','product','industry_fsm_sale','custom_stock'],
+    'depends': ['base',
+                'sale',
+                'sale_management',
+                'product',
+                'industry_fsm_sale',
+                'custom_stock',
+                'account_payment',
+                ],
 
     # always loaded
     'data': [
