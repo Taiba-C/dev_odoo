5804804425aebb2f7ca03fb978f5f3f5f949e7e3
Tsiky <tsiky@test.com>
2023-01-04 11:24:37 +0300

    fix warning on mrp

--- custom_manufacturing/models/mrp_bom.py
+++ custom_manufacturing/models/mrp_bom.py
@@ -5,18 +5,19 @@ from odoo import models, fields, api, _
 class Mrp_bom(models.Model):
     _inherit = "mrp.bom"    
     
-    # @api.model
-    def create(self, vals):
+    @api.model_create_multi
+    def create(self, val_lists):
         """
             change product template and product product as bom parent
         """
-        res = super(Mrp_bom, self).create(vals)
-        
-        self.env['product.template'].search([('id', '=', vals['product_tmpl_id'])]).write({'is_bom_parent': True})
-        self.env['product.product'].search([('product_tmpl_id', '=', vals['product_tmpl_id'])]).write({'is_bom_parent': True})
-        
+        for vals in val_lists:
+            res = super(Mrp_bom, self).create(vals)
             
-        return res
+            self.env['product.template'].search([('id', '=', vals['product_tmpl_id'])]).write({'is_bom_parent': True})
+            self.env['product.product'].search([('product_tmpl_id', '=', vals['product_tmpl_id'])]).write({'is_bom_parent': True})
+            
+                
+            return res
     
     def unlink(self):
         """
