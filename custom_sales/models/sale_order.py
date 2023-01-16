e4f710df9941e6bb39bfdfbac854d7e03b5ad163
Tsiky <tsiky@test.com>
2023-01-16 11:10:59 +0300

    clean code

--- custom_sales/models/sale_order.py
+++ custom_sales/models/sale_order.py
@@ -27,8 +27,6 @@ class Sale_order(models.Model):
         
         for line in self.order_line:
             if line.product_id:
-                # print(line.product_id.product_tmpl_id.id)
-                # last_product = len(line.product_id) -1
                 
                 boms = self.get_product_bom(line.product_id.product_tmpl_id.id)
                 
