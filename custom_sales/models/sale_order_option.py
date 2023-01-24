27a743002d893fe3b54e37eb4cfe524ccc8e711a
Tsiky <tsiky@test.com>
2023-01-24 17:23:05 +0300

    clean code

--- custom_sales/models/sale_order_option.py
+++ custom_sales/models/sale_order_option.py
@@ -21,46 +21,6 @@ class SaleOrderOption(models.Model):
     total_purchase_price = fields.Float('Total purchase price', readonly=True)
     
     total_sale_price = fields.Float('Total sale price', readonly=True)
-        
-    # @api.onchange('purchase_price')
-    # def _onchange_purchase_price(self):
-    #     self.update_option_line()
     
-    # @api.onchange('quantity')
-    # def _onchange_quantity(self):
-    #     self.update_option_line()
-
-    
-    # @api.onchange('margin_product')
-    # def _onchange_margin_product(self):
-    #     self.update_option_line()
-        
-    # def update_option_line(self):
-    #     """
-    #         update total on change
-    #     """
-    #     if self.product_id:
-    #         if self.product_id.detailed_type != 'service':
-    #             # prix total achat
-    #             self.total_purchase_price = self.purchase_price * self.quantity
-                
-    #             # prix total vente
-    #             if self.margin_product > 0:
-    #                 self.total_sale_price = self.total_purchase_price / self.margin_product
-                    
-    #             if self.margin_product == 0 :
-    #                 raise UserError("You cannot set this value to margin as 0!")
-    #             if self.margin_product > 1 :
-    #                 raise UserError("You cannot set this value up to 1!")
-                    
-                
-    #             # marge en €
-    #             self.margin = self.total_sale_price - self.total_purchase_price
-    #             if self.margin < 0:
-    #                 self.margin = 0
-                
-    #             # marge %
-    #             if self.total_sale_price > 0:
-    #                 self.margin_percent = self.margin / self.total_sale_price
                 
     
