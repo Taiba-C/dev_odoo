b6a11850192ac19f803de42e187f5676478ce626
Tsiky <tsiky@test.com>
2023-01-25 14:20:44 +0300

    fix and delete field

--- custom_sales/models/sale_order.py
+++ custom_sales/models/sale_order.py
@@ -15,9 +15,6 @@ class Sale_order(models.Model):
     
     date_of_exhibition = fields.Date('Begin of exhibition')
     
-    validity_quotation = fields.Date('Validity of the quotation')
-
-    
     
     def generate_bom_order(self):
         """
