82fb8b5323b1843a7110a6b921612f61b409feb2
Tsiky <tsiky@test.com>
2023-02-07 15:58:20 +0300

    change status from action_confirm and create invoices

---
+++ custom_crm/models/sale_order.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Sale_order(models.Model):
+    _inherit = 'sale.order'
+    
+    def action_confirm(self):
+        res = super(Sale_order,self).action_confirm()
+        
+        self.opportunity_id.write({'stage_id':3})
+        
+        return res
+    
+
+class SaleAdvancePaymentInv(models.TransientModel):
+    _inherit = 'sale.advance.payment.inv'
+
+    @api.model
+    def _create_invoices(self, sale_orders):        
+        invoices = super(SaleAdvancePaymentInv, self.with_context(self._context))._create_invoices(sale_orders)
+        order = self.env['sale.order'].search([('id','=', self._context['active_id'])])
+        print(order.invoice_ids)
+        if len(order.invoice_ids) == 1:
+            # check state of opportunity
+            order.opportunity_id.write({'stage_id':4})
+    
+        
+        return invoices
