688c150a3354705a35542cb86453fbc0b737df65
Tsiky <tsiky@test.com>
2023-02-08 17:35:08 +0300

    set name of BOM in task

--- custom_sales/models/sale_order_option.py
+++ custom_sales/models/sale_order_option.py
@@ -27,7 +27,7 @@ class SaleOrderOption(models.Model):
         for record in self:
             if record.product_id.type == 'service':
                 task = self.env['project.task'].create({
-                    'name': record.product_id.name,
+                    'name': record.product_id.name + ' ' + record.parent_id.name,
                     'project_id': project.id,
                     'partner_id': partner_id,
                     'planned_hours': record.quantity,
