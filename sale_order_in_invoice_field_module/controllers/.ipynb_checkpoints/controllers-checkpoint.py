691f36b008657537f4fb3fa00c1b2c6cb2cf5ec2
Mamadou <mamadou.dione@axeneo.com>
2023-03-20 09:22:26 +0000

    Test ajout du bon de commande sur la facture

---
+++ sale_order_in_invoice_field_module/controllers/.ipynb_checkpoints/controllers-checkpoint.py
@@ -0,0 +1,21 @@
+-*- coding: utf-8 -*-
+from odoo import http
+
+
+class SaleOrderInInvoiceFieldModule(http.Controller):
+    @http.route('/sale_order_in_invoice_field_module/sale_order_in_invoice_field_module', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
+
+    @http.route('/sale_order_in_invoice_field_module/sale_order_in_invoice_field_module/objects', auth='public')
+    def list(self, **kw):
+        return http.request.render('sale_order_in_invoice_field_module.listing', {
+            'root': '/sale_order_in_invoice_field_module/sale_order_in_invoice_field_module',
+            'objects': http.request.env['sale_order_in_invoice_field_module.sale_order_in_invoice_field_module'].search([]),
+            })
+
+@http.route('/sale_order_in_invoice_field_module/sale_order_in_invoice_field_module/objects/<model("sale_order_in_invoice_field_module.sale_order_in_invoice_field_module"):obj>', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('sale_order_in_invoice_field_module.object', {
+            'object': obj
+        })
---
+++ sale_order_in_invoice_field_module/models/models.py
@@ -0,0 +1,16 @@
+-*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+from odoo.exceptions import UserError
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+    _description = 'Account Move'
+
+    @api.depends('invoice_origin')
+    def _compute_sale_order(self):
+        for record in self:
+            sale_order = self.env['sale.order'].search([('name','=',record.invoice_origin)], limit=1)
+            record.x_studio_bc = sale_order.id if sale_order else False
+
+    x_studio_bc = fields.Many2one('sale.order', string='Sale Order', compute='_compute_sale_order')
