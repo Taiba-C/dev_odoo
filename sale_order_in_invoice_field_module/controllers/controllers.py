b407c9c1fdc997016786dd9fcd4604c9d998bce0
Mamadou <mamadou.dione@axeneo.com>
2023-03-20 10:52:41 +0000

    Test

--- sale_order_in_invoice_field_module/controllers/controllers.py
+++ sale_order_in_invoice_field_module/controllers/controllers.py
@@ -14,9 +14,8 @@ class SaleOrderInInvoiceFieldModule(http.Controller):
             'root': '/sale_order_in_invoice_field_module/sale_order_in_invoice_field_module',
             'objects': http.request.env['sale_order_in_invoice_field_module.sale_order_in_invoice_field_module'].search([]),
             })
-
-@http.route('/sale_order_in_invoice_field_module/sale_order_in_invoice_field_module/objects/<model("sale_order_in_invoice_field_module.sale_order_in_invoice_field_module"):obj>', auth='public')
-    def object(self, obj, **kw):
+    @http.route('/sale_order_in_invoice_field_module/sale_order_in_invoice_field_module/objects/<model("sale_order_in_invoice_field_module.sale_order_in_invoice_field_module"):obj>', auth='public')
+    def object(self, obj, **kw):              
         return http.request.render('sale_order_in_invoice_field_module.object', {
             'object': obj
-        })
+            })
