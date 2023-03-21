c52c8ee37a50acd632342c772258f41f1d92c417
Mamadou <mamadou.dione@axeneo.com>
2023-03-21 13:47:09 +0000

    accountinvoice to account.move

--- sale_order_in_invoice_field_module/models/models.py
+++ sale_order_in_invoice_field_module/models/models.py
@@ -1,31 +1,16 @@
 #-*- coding: utf-8 -*-
-# Version qui fonctionne mais créer un bug car probablement le module compta pas installer
-#from odoo import api, fields, models
-#from odoo.exceptions import UserError
-
-#class AccountMove(models.Model):
- #   _inherit = 'account.move'
-  #  _description = 'Account Move'
-
-   # @api.depends('invoice_origin')
-    #def _compute_sale_order(self):
-     #   for record in self:
-      #      sale_order = self.env['sale.order'].search([('name','=',record.invoice_origin)], limit=1)
-     #       record.x_studio_bc = sale_order.id if sale_order else False
-
-   # x_studio_bc = fields.Many2one('sale.order', string='Sale Order', compute='_compute_sale_order')
-#
+# Version qui fonctionne mais créer un bug car probablement le module compta pas installer( à corriger)
 from odoo import api, fields, models
 from odoo.exceptions import UserError
 
-class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
-    _description = 'Account Invoice'
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+    _description = 'Account Move'
 
-    @api.depends('origin')
+    @api.depends('invoice_origin')
     def _compute_sale_order(self):
         for record in self:
-            sale_order = self.env['sale.order'].search([('name','=',record.origin)], limit=1)
+            sale_order = self.env['sale.order'].search([('name','=',record.invoice_origin)], limit=1)
             record.x_studio_bc = sale_order.id if sale_order else False
 
     x_studio_bc = fields.Many2one('sale.order', string='Sale Order', compute='_compute_sale_order')
