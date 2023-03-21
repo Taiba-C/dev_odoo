0f7bc7120d00d180539a2351d76dc4528ffe2392
Mamadou <mamadou.dione@axeneo.com>
2023-03-21 09:19:10 +0000

    modif licence

--- sale_order_in_invoice_field_module/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ sale_order_in_invoice_field_module/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -21,7 +21,8 @@
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
-
+    
+    'license': 'LGPL-3',
     # always loaded
     'data': [
         'security/ir.model.access.csv',
