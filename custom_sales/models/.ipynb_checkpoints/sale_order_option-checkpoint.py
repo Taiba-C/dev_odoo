65409c6dc82c76f2ce1adf4ad01099cb17780345
Mamamdou <mamadou.dione@axeneo.com>
2023-04-12 08:20:56 +0000

    planning_slot_task

--- custom_sales/models/.ipynb_checkpoints/sale_order_option-checkpoint.py
+++ custom_sales/models/.ipynb_checkpoints/sale_order_option-checkpoint.py
@@ -45,12 +45,15 @@ class SaleOrderOption(models.Model):
                     'project_id': project.id,
                     'start_datetime': date_start,
                     'end_datetime': date_start + timedelta(hours=overtime),
+                    'task_id': task.id
                 })   
                 record.planning_id = planning
                 
                 
 class Planning_slot(models.Model):
     _inherit = 'planning.slot'
+    
+    task_id = fields.Many2one('project.task', string='Task', ondelete='cascade')
     
     @api.depends('start_datetime', 'end_datetime', 'resource_id.calendar_id',
         'company_id.resource_calendar_id', 'allocated_percentage', 'resource_id.flexible_hours')
