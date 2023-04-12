28e3cc94668ae08ecb2972ae852cdd405fa69af3
Mamamdou <mamadou.dione@axeneo.com>
2023-04-12 01:05:07 +0000

    task

--- custom_sales/models/.ipynb_checkpoints/sale_order_option-checkpoint.py
+++ custom_sales/models/.ipynb_checkpoints/sale_order_option-checkpoint.py
@@ -45,6 +45,8 @@ class SaleOrderOption(models.Model):
                     'project_id': project.id,
                     'start_datetime': date_start,
                     'end_datetime': date_start + timedelta(hours=overtime),
+                    'job_title': record.product_id.name + ' ' + record.order_line_id.product_id.name,
+                    'name': task.name
                 })   
                 record.planning_id = planning
                 
