6f4b4d013646c756c1a31fe676cd9621ad118eed
Falilou NIANG <falilou.niang@agilicis.com>
2023-08-08 16:26:58 +0000

    [Bug fixed[  Opening and ending tag mismatch error

--- custom_sales/models/devise.py
+++ custom_sales/models/devise.py
@@ -2,9 +2,10 @@
 from odoo import _, fields, models, api
 
 class Remise(models.Model):
-    _name = 'fn.remise'
+    _name = 'nesil.remise'
     _rec_name='remise'
     
     remise = fields.Char(string="Nom de remise")
     taux_de_remise = fields.Float(string='Taux de remise')
-    date_de_remise = 
+    date_de_remise = fields.Date(string='Date de remise')
+    active = fields.Boolean(string='Active')
