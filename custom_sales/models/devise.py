d7f2b2bc7072f53db463b1de49121dd6054d0bc0
Falilou NIANG <falilou.niang@agilicis.com>
2023-08-08 16:07:44 +0000

    [ADD] replace somes x_studio fields in custom modules

--- custom_sales/models/devise.py
+++ custom_sales/models/devise.py
@@ -5,5 +5,6 @@ class Remise(models.Model):
     _name = 'fn.remise'
     _rec_name='remise'
     
-    remise = fields.float('res.currency', string="remise")
+    remise = fields.Char(string="Nom de remise")
     taux_de_remise = fields.Float(string='Taux de remise')
+    date_de_remise = 
