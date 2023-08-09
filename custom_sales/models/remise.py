bedc52937ed9cef7263bbe9da21cb3f531f02fc0
Falilou NIANG <falilou.niang@agilicis.com>
2023-08-09 12:43:04 +0000

    [Bug fixed[

--- custom_sales/models/remise.py
+++ custom_sales/models/remise.py
@@ -2,7 +2,7 @@
 from odoo import _, fields, models, api
 
 class Remise(models.Model):
-    _name = 'custom_sales.remise'
+    _name = 'nesil.remise'
     
     name = fields.Char(string="Nom de remise") 
     taux_de_remise = fields.Float(string='Taux de remise')
