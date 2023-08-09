7e9c4b3032c360354ffffbd3c3db16819c4b130d
Falilou NIANG <falilou.niang@agilicis.com>
2023-08-09 14:37:07 +0000

    [ADD] discount_limit function impl

--- custom_sales/models/sale_order.py
+++ custom_sales/models/sale_order.py
@@ -596,12 +596,15 @@ class SaleOrderLine(models.Model):
     def _check_discount_limit(self):
         allowed_department = 'Direction'
         discount = self.env['nesil.remise'].sudo().search([])
-        if discount.ensure_one():
-            if discount.active:
-                for order in self:
-                    user_department = order.env.user.employee_id.department_id.name
-                    if order.discount > discount.taux_de_remise*100 and user_department != allowed_department:
-                        raise models.ValidationError("Vous n'avez pas l'autorisation requise pour attribuer une remise supérieure à "+str(discount.taux_de_remise*100)+"%")
+        if discount:
+            if discount.ensure_one():
+                if discount.active:
+                    for order in self:
+                        user_department = order.env.user.employee_id.department_id.name
+                        if order.discount > discount.taux_de_remise*100 and user_department != allowed_department:
+                            raise models.ValidationError("Vous n'avez pas l'autorisation requise pour attribuer une remise supérieure à "+str(discount.taux_de_remise*100)+"%")
+                else:
+                    raise models.ValidationError("Impossible d'appliquer une remise")
             else:
                 raise models.ValidationError("Impossible d'appliquer une remise")
         else:
