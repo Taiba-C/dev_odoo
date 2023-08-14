0c68d7061f0d254ae7db9e512d1e38457045df1f
Falilou NIANG <falilou.niang@agilicis.com>
2023-08-14 15:02:38 +0000

    [ADD] fixed some errors

--- custom_sales/models/sale_order.py
+++ custom_sales/models/sale_order.py
@@ -12,7 +12,7 @@ class Sale_order(models.Model):
     total_sale = fields.Float('Total sale price', readonly = True,compute="_compute_total_infos")
     margin = fields.Float('Margin', readonly = True, compute="_compute_total_infos")
     margin_percent = fields.Float('Margin %', readonly = True, compute="_compute_total_infos")
-    rfrence_du_dossier = fields.Many2one('sale.order',string='Référence du dossier',related='origin')
+    rfrence_du_dossier = fields.Many2one('sale.order',string='Référence du dossier')
     version_du_devis = fields.Char(string='Version du devis')
     
     date_of_exhibition = fields.Date('Begin of exhibition')
