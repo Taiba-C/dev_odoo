7fb98065776722e6918c8e02e9ff11166294a8d8
Falilou NIANG <falilou.niang@agilicis.com>
2023-08-18 16:36:58 +0000

    [BUG] fixed

--- custom_crm/models/crm_lead.py
+++ custom_crm/models/crm_lead.py
@@ -24,6 +24,11 @@ class Lead(models.Model):
     date_de_demontage_du = fields.Date(string='Date de démontage Du')
     date_de_demontage_au = fields.Date(string='Au')
     warning_copy = fields.Boolean(default=False)
+    warning_display_time = fields.Integer()
+    def check_if_display_warning(self):
+        for rec in self:
+            if rec.warning_display_time > 1 and rec.warning_copy:
+                rec.warning_copy = False
 
     
     def action_sale_quotations_new(self):
@@ -57,5 +62,6 @@ class Lead(models.Model):
                 res.order_ids.action_cancel()
                 if res.dbut_salon - date.today() > timedelta(0):
                     res.warning_copy = True
+                    res.warning_display_time = 1
         return res
         
