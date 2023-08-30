9f58c187ca17420234baa4659f1b0fcd5847f4d3
Falilou NIANG <falilou.niang@agilicis.com>
2023-08-30 16:29:52 +0000

    [ADD] copy function in sale.order.option

--- custom_sales/models/sale_order_option.py
+++ custom_sales/models/sale_order_option.py
@@ -20,7 +20,7 @@ class SaleOrderOption(models.Model):
     
     total_purchase_price = fields.Float('Total purchase price', readonly=True,copy=True)
     
-    order_line_id = fields.Many2one('sale.order.line', string='Order Line',ondelete='cascade',copy=True)
+    order_line_id = fields.Many2one('sale.order.line', string='Order Line',ondelete='cascade',copy=False)
     
     total_sale_price = fields.Float('Total sale price', readonly=True,copy=True)
     task_id = fields.Many2one('project.task', string='Task', ondelete='cascade')
