515932efd116044dbf173be6fdfb312445171447
Falilou NIANG <falilou.niang@agilicis.com>
2023-08-30 20:51:01 +0000

    [ADD] copy function in sale.order.option

--- custom_sales/models/sale_order.py
+++ custom_sales/models/sale_order.py
@@ -24,7 +24,7 @@ class Sale_order(models.Model):
                 rec.rfrence_du_dossier = None
             order_line_ids = self.env['sale.order.line'].search([('order_id','=',rec.id)])
             sale_order_option_ids = self.env['sale.order.option'].search([('order_id','=', rec.id)])
-            if len(sale_order_option_ids) > 1:
+            if len(sale_order_option_ids) > 0:
                 for order_option in sale_order_option_ids:
                     product_name = order_option.order_line_id.name.split('-')[1]
                     old_line = rec.name+' - '+product_name
