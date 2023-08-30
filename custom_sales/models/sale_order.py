1dc36296f5294fc1eda49f91bf7379b9f18131fa
Falilou NIANG <falilou.niang@agilicis.com>
2023-08-30 21:22:01 +0000

    [ADD] copy function in sale.order.option

--- custom_sales/models/sale_order.py
+++ custom_sales/models/sale_order.py
@@ -33,6 +33,8 @@ class Sale_order(models.Model):
                             for line in order_line_ids:
                                 if product_name in line.name:
                                     order_option.order_line_id = line.id
+                                else:
+                                    raise UserError("Test! ",product_name)
 
     rfrence_du_dossier = fields.Many2one('crm.lead',string='Référence du dossier',compute='get_ref_dossier')
     version_du_devis = fields.Char(string='Version du devis')
