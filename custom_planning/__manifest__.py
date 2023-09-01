acd559f82a93360a2d3c1b5b7703a0ccaa0bf72b
Falilou NIANG <falilou.niang@agilicis.com>
2023-09-01 12:59:42 +0000

    [ADD] default filter removed

--- custom_planning/__manifest__.py
+++ custom_planning/__manifest__.py
@@ -28,7 +28,8 @@
         # 'security/ir.model.access.csv',
         #'views/views.xml',
         #'views/templates.xml',
-        'views/planning.xml',
+        #'views/planning.xml',
+        'views/replaced_workorder.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
