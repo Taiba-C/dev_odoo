4d005a2264c38231e7e036de8585c0c1445a3c71
khobilem <falilou.niang@agilicis.com>
2023-10-09 15:04:29 +0000

    Update sale_order_option_nesil.py

--- custom_sales/models/sale_order_option_nesil.py
+++ custom_sales/models/sale_order_option_nesil.py
@@ -292,9 +292,9 @@ class SaleOrderOptionNesil(models.Model):
     
     def retro_options_nesil_options(self):
         orders = self.env["sale.order"].sudo().search([])
-        order_name = ['NE202300320']
+        order_name = ['NE202300328']
         for order in orders:
-            if len(order.sale_order_nesil_option_ids) <= 0 and len(order.sale_order_option_ids) > 0 and order.name in order_name:
+            if len(order.sale_order_nesil_option_ids) <= 0 and len(order.sale_order_option_ids) > 0 and order.name not in order_name:
                 for option in order.sale_order_option_ids:
                     self.env["sale.order.option.nesil"].sudo().create({'product_id':option.product_id.id,'line_id':option.line_id.id,'name':option.name,
                         'quantity':option.quantity,'uom_id':option.uom_id.id,'product_uom_category_id':option.product_uom_category_id.id,
