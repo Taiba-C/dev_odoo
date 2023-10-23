f4def4eed9e9b4cb2fea00dd6ee75e5ee0be22c1
khobilem <falilou.niang@agilicis.com>
2023-10-23 15:19:10 +0000

    Update __manifest__.py

--- custom_sales/__manifest__.py
+++ custom_sales/__manifest__.py
@@ -58,7 +58,7 @@
         'views/wizard_component_bom.xml',
         'views/wizard_component_bom_option.xml',
         'views/remise.xml',
-        'report/options.xml',
+        #'report/options.xml',
         'report/portal_options.xml',
         'views/menu.xml',
         'data/data_workcenter.xml',
