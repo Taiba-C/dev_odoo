649a42ce3616640150f621525ec06720dcaaff3e
ares <ranoaritsiky@gmail.com>
2024-02-16 19:08:14 +0300

    [FIX] set subcontractor not readonly

--- custom_sales/models/res_partner.py
+++ custom_sales/models/res_partner.py
@@ -8,5 +8,4 @@ from odoo import api, models, fields
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    is_subcontractor = fields.Boolean('Est un sous-traitant')
     is_subcontractors = fields.Boolean('Est un sous-traitant')
