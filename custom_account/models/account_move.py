11fc00cea0c44cc45a9a4fb0ddb55a28abf693ff
ares <ranoaritsiky@gmail.com>
2024-06-12 09:11:44 +0300

    [FIX] check if account move is not false to avoid error when send mail

--- custom_account/models/account_move.py
+++ custom_account/models/account_move.py
@@ -201,11 +201,13 @@ class AccountMove(models.Model):
         
     def get_account_move(self, account_move_id):
         account_move = self.env['account.move'].search([('id','=', account_move_id)], limit=1)
-        related_quotations = self.env['sale.order'].search([
-                                        ('name', 'in', account_move.invoice_origin.split(', ')),  # Split if multiple references are stored
-                                        ('state', '!=', 'cancel'),  # Exclude canceled sale orders if needed
-                                    ])
-        return {
-            "opportunity_name": related_quotations[0].opportunity_id.name,
-            "signature_malika": self.env['res.users'].search([('name', 'ilike', 'malika')])
-        }
+        if account_move.invoice_origin:
+            related_quotations = self.env['sale.order'].search([
+                                            ('name', 'in', account_move.invoice_origin.split(', ')),  # Split if multiple references are stored
+                                            ('state', '!=', 'cancel'),  # Exclude canceled sale orders if needed
+                                        ])
+            if related_quotations:
+                return {
+                    "opportunity_name": related_quotations[0].opportunity_id.name,
+                    "signature_malika": self.env['res.users'].search([('name', 'ilike', 'malika')])
+                }
