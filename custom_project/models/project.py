d80a2682a054cbee363833a09fc79dfa7b35a194
ares <ranoaritsiky@gmail.com>
2024-07-04 09:02:17 +0300

    [FIX] update calcul not done

--- custom_project/models/project.py
+++ custom_project/models/project.py
@@ -99,18 +99,14 @@ class ProjectProject(models.Model):
             else:
                 record.work_order_percentage_done = 0.0
 
+        
     @api.depends('order_id')
     def _compute_work_order_percentage_not_done(self):
         """
         Compute the percentage of work orders that are not done for each record.
 
-        This method calculates the percentage of work orders that are not done for each record in the model.
-        It iterates over the records and checks if there is an associated order. If there is, it retrieves the
-        manufacturing orders related to that order and calculates the completed and total count of work orders.
-        It then calculates the percentage of work orders that are not done and assigns it to the
-        `work_order_percentage_not_done` field of the record.
-
-        If there is no associated order, the `work_order_percentage_not_done` field is set to 0.0.
+        This method calculates the percentage of work orders that are in the 'waiting' or 'pending' state
+        out of the total number of work orders associated with the record's sale order.
 
         :return: None
         """
@@ -118,25 +114,18 @@ class ProjectProject(models.Model):
             if record.order_id:
                 if record.order_id and record.order_id.state in ['draft', 'sent', 'sale']:
                     manufacturing_orders = self.env['mrp.production'].search([('sale_order', '=', record.order_id.id)])
-                    total_completed_count = 0
+                    total_draft_pending_count = 0
                     total_total_count = 0
                     for order in manufacturing_orders:
                         work_order_states = [work_order.state for work_order in order.workorder_ids]
-                        completed_count = work_order_states.count('done')
+                        draft_pending_count = work_order_states.count('waiting') + work_order_states.count('pending')
                         total_count = len(work_order_states)
-                        total_completed_count += completed_count
+                        total_draft_pending_count += draft_pending_count
                         total_total_count += total_count
                     if total_total_count > 0:
-                        record.work_order_percentage_done = (total_completed_count / total_total_count)
-                    else:
-                        record.work_order_percentage_done = 0.0
-
-                    not_done_count = total_total_count - total_completed_count
-                    if total_total_count > 0:
-                        record.work_order_percentage_not_done = (not_done_count / total_total_count)
+                        record.work_order_percentage_not_done = (total_draft_pending_count / total_total_count)
                     else:
                         record.work_order_percentage_not_done = 0.0
-
             else:
                 record.work_order_percentage_not_done = 0.0
 
